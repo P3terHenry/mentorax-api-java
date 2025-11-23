0429d4faf1fbcde161efdb7e5b9466fe51792d27
P3terHenry <pedro.antonieti@outlook.com>
2025-11-23 05:28:10 -0300

    :sparkles: Adiciona método findAllUsuarios no UsuarioRepository para retornar lista de usuários como UsuarioDTO

--- src/main/java/br/com/fiap/gs/mentorax/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/gs/mentorax/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package br.com.fiap.gs.mentorax.repository;
 
+import br.com.fiap.gs.mentorax.dto.UsuarioDTO;
 import br.com.fiap.gs.mentorax.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +15,18 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Query("""
+    SELECT new br.com.fiap.gs.mentorax.dto.UsuarioDTO(
+        u.idUsuario,
+        u.nome,
+        u.email,
+        u.tipoUsuario,
+        u.cargo,
+        u.dataCadastro,
+        u.ativo
+    )
+    FROM Usuario u
+    """)
+    List<UsuarioDTO> findAllUsuarios();
 }
